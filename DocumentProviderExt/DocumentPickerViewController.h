414103e906a6128d18920e1d901b1da04986c1d8
ikuya <ikuya@ssag-mbp-iossec.local>
2014-10-15 18:22:55 +0900

    add Document Provider Extension with iCloud capability

---
+++ DocumentProviderExt/DocumentPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  DocumentPickerViewController.h
+//  DocumentProviderExt
+//
+//  Created by Ikuya Fukumoto on 10/15/14.
+//  Copyright (c) 2014 if. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DocumentPickerViewController : UIDocumentPickerExtensionViewController
+
+@end
---
+++ DocumentProviderExtFileProvider/FileProvider.h
@@ -0,0 +1,13 @@
+//
+//  FileProvider.h
+//  DocumentProviderExtFileProvider
+//
+//  Created by Ikuya Fukumoto on 10/15/14.
+//  Copyright (c) 2014 if. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileProvider : NSFileProviderExtension
+
+@end
